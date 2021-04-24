61a32b8b9e40ea6f0e6472cc387f4f15f4a125ba
vishi12 <77247243+vishi12@users.noreply.github.com>
2021-04-24 19:09:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function addUser() {
+    player1_name = document.getElementById("player1_input").value;
+    player2_name = document.getElementById("player2_input").value;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player1_name", player1_name);
+
+    window.location = "game_page.html";
+}
